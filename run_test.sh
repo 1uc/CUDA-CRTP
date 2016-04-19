0f5c895d3ab83cc0a0455ab6d3ed8a234c23af8b
Luc Grosheintz <luc.grosheintz@gmail.com>
2016-04-19 17:43:16 +0200

    Simplify reproduction script.

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/env bash
 
-log="cuda-crtp.log"
-
 make all
 
-nvcc --version   | tee ${log}
-nvidia-smi       | tee --append ${log}
-
-echo "---- ./with-user-cast --------------------------"   | tee --append ${log}
-./with-user-cast            | tee --append ${log}
-echo "---- ./no-user-cast ----------------------------"   | tee --append ${log}
-./no-user-cast              | tee --append ${log}
+echo "---- ./with-user-cast --------------------------"
+./with-user-cast
+echo "---- ./no-user-cast ----------------------------"
+./no-user-cast
